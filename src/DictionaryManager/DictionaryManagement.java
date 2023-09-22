6ae6f9cdd16dae554677c2f27c9a4b2a51302dfb
giakhanh22024558 <127011768+giakhanh22024558@users.noreply.github.com>
2023-09-22 22:49:18 +0700

    Add files via upload

---
+++ src/DictionaryManager/DictionaryManagement.java
@@ -0,0 +1,23 @@
+package DictionaryManager;
+
+import java.util.Scanner;
+
+public class DictionaryManagement {
+
+    public DictionaryManagement() {
+    }
+
+    /****
+     *
+     */
+    public Word insertFromCommandLine() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your input with foramt: word_target <enter> word_explain");
+        String word_target = sc.nextLine();
+        String word_explain = sc.nextLine();
+
+        Word word = new Word(word_target, word_explain);
+
+        return word;
+    }
+}
