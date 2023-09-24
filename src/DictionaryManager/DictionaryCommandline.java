cc6f408d1ac5e2243be21f27e4909281d6403a2b
MinhSpaceX <126952602+MinhSpaceX@users.noreply.github.com>
2023-09-24 11:06:37 +0700

    Optimize code

--- src/DictionaryManager/DictionaryCommandline.java
+++ src/DictionaryManager/DictionaryCommandline.java
@@ -3,17 +3,29 @@ package DictionaryManager;
 import java.util.ArrayList;
 
 public class DictionaryCommandline {
+    void headMatch() {
 
-    public DictionaryCommandline() {
     }
 
-    /****
+    /**
      * Function to print all contents of a dictionary.
      * @param dict dictionary to print out.
      */
-    public void ShowAllWords(ArrayList<Word> dict) {
+    public static void ShowAllWords(ArrayList<Word> dict) {
         for (Word w : dict) {
             System.out.format("| %s | %s", w.GetWordTarget(), w.GetWordExplain());
         }
+    }
+
+    /**
+     * Function to search for words start with the key value.
+     * @param key : value to pass in.
+     * @return 0: No word found.
+     * <p>
+     * 1: One or many words found.
+     */
+    public static int dictionarySearcher(String key) {
+        
+        return 0;
     }
 }
