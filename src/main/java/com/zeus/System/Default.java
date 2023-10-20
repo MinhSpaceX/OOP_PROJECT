a4595cdfc718552a4f5ece2d266f9713d0cc8c62
MinhSpaceX <126952602+MinhSpaceX@users.noreply.github.com>
2023-10-20 22:20:45 +0700

    Delete src directory

--- src/main/java/com/zeus/System/Default.java
+++
@@ -1,6 +0,0 @@
-package com.zeus.System;
-
-public class Default {
-    static final String DATABASE_PATH_TXT = "com/zeus/data/dictionary.txt";
-    static final String DATABASE_PATH_DB = "resource/data/Dictionary.db";
-}
