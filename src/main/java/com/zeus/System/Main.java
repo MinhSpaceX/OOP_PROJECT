6ceaea872a4e3fcd1a8f7f87b84995d81394152b
MinhSpaceX <126952602+MinhSpaceX@users.noreply.github.com>
2023-10-21 16:29:03 +0700

    Implement trie structure

--- src/main/java/com/zeus/System/Main.java
+++ src/main/java/com/zeus/System/Main.java
@@ -1,18 +1,32 @@
 package com.zeus.System;
 
 import com.zeus.utils.file.FileManager;
+import com.zeus.utils.trie.Trie;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
         // Shutdown function when the program end.
-        long startTime = System.currentTimeMillis();
+        /*long startTime = System.currentTimeMillis();
         String url1 = "/com/zeus/data/dictionary.json";
         String url2 = "/com/zeus/data/english-vietnamese.json";
         int words = 10;
         Objects.requireNonNull(FileManager.loadWord(url2, words)).forEach(word -> System.out.println(word.toString()));
         long endTime = System.currentTimeMillis();
-        System.out.printf("Execution Time: %s Millisecond",endTime-startTime);
+        System.out.printf("Execution Time: %s Millisecond\n",endTime-startTime);*/
+
+        Trie trie = new Trie();
+        trie.insert("cat");
+        trie.insert("cats");
+        trie.insert("catse");
+        trie.insert("catses");
+        trie.delete("cats");
+        trie.search("cat");
+        trie.search("cats");
+        trie.search("catse");
+        trie.search("catses");
     }
 }
---
+++ src/main/java/com/zeus/utils/trie/Trie.java
@@ -0,0 +1,127 @@
+package com.zeus.utils.trie;
+
+import com.zeus.utils.log.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Trie {
+    private static final int ALPHABET_SIZE = 26;
+    private Node root = null;
+    public Trie() {
+        root = new Node();
+    }
+    public boolean insert(String word) {
+        if (root == null) {
+            Logger.warn("Root is null.");
+        }
+        if (word == null) return false;
+        word = word.toLowerCase();
+        int index = 0;
+        Node iterator = root;
+        for (int character = 0; character < word.length(); character++) {
+            index = word.charAt(character);
+            if (iterator.getChildren().get(index) == null) {
+                iterator.add(index);
+            }
+            iterator = iterator.next(index);
+        }
+        if (iterator.isEndOfWord()) {
+            Logger.warn("Insert action cancelled. Word existed!");
+            return false;
+        }
+        iterator.setEndOfWord(true);
+        return true;
+    }
+
+    public boolean search(String word) {
+        if (isEmpty()) {
+            Logger.warn("Trie is empty.");
+            return false;
+        }
+        word = word.toLowerCase();
+        int index = 0;
+        Node iterator = root;
+        for (int character = 0; character < word.length(); character++) {
+            index = word.charAt(character);
+            if (iterator.getChildren().get(index) == null) {
+                return false;
+            }
+            iterator = iterator.next(index);
+        }
+        if (iterator.isEndOfWord()) {
+            Logger.info("Found word: "+ word);
+        }
+        return iterator.isEndOfWord();
+    }
+
+    public boolean delete(String word) {
+        if (isEmpty()) {
+            Logger.warn("Trie is empty.");
+            return false;
+        }
+        if (delete(root, word, 0)) {
+            Logger.info("Delete successfully.");
+            return true;
+        }
+        return false;
+    }
+
+    private boolean delete(Node node, String word, int depth) {
+        if (node == null) {
+            Logger.warn("Delete word cancelled. Word does not exist!");
+            return false;
+        }
+        if (word.length()-1 == depth) {
+            if (!node.isEndOfWord()) {
+                Logger.warn("Delete word cancelled. Word does not exist!");
+                return false;
+            }
+            return node.getChildren().isEmpty();
+        }
+        if (delete(node.getChildren().get((int)word.charAt(depth)), word, depth+1)) {
+            node.getChildren().remove((int)word.charAt(depth));
+        }
+        return false;
+    }
+
+    public void reset() {
+        root.children.clear();
+    }
+
+    public boolean isEmpty() {
+        return root.getChildren().isEmpty() || root == null;
+    }
+    static class Node {
+        private Map<Integer, Node> children = new HashMap<>();
+        private boolean endOfWord = false;
+
+        public Node() {}
+        public Node next(int character) {
+            return children.get(character);
+        }
+
+        public void add(int character) {
+            children.put(character, new Node());
+        }
+        public Node(boolean endOfWord) {
+            this.endOfWord = endOfWord;
+        }
+
+        public Map<Integer, Node> getChildren() {
+            return children;
+        }
+
+        public void setChildren(Map<Integer, Node> children) {
+            this.children = children;
+        }
+
+        public boolean isEndOfWord() {
+            return endOfWord;
+        }
+
+        public void setEndOfWord(boolean endOfWord) {
+            this.endOfWord = endOfWord;
+        }
+    }
+}
