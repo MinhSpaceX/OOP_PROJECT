dbd08a387a5e0e4155b0e9e9b7d2dd0a9f6963c9
MinhSpaceX <126952602+MinhSpaceX@users.noreply.github.com>
2023-11-20 14:57:43 +0700

    Finish

--- src/main/java/com/zeus/Main/Main.java
+++ src/main/java/com/zeus/Main/Main.java
@@ -1,17 +1,12 @@
 package com.zeus.Main;
 
-import com.zeus.App.Controller.FavoriteController;
-import com.zeus.App.Controller.History;
 import com.zeus.App.Window.App;
-import com.zeus.utils.file.FileManager;
 import com.zeus.utils.log.Logger;
 import com.zeus.utils.managerfactory.ManagerFactory;
 import com.zeus.Managers.SystemApp.SystemManager;
 
-import java.lang.reflect.InvocationTargetException;
-
 public class Main {
-    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException {
+    public static void main(String[] args) {
         // Shutdown function when the program end.
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             SystemManager.getManager(SystemManager.class).terminate();
