44e2aa186531d89fb518f32d4651d617ac852beb
HungPhan-TCLC <22024566@vnu.edu.vn>
2023-11-20 09:13:16 +0700

    Translate

--- src/main/java/com/zeus/App/Controller/Translate.java
+++ src/main/java/com/zeus/App/Controller/Translate.java
@@ -2,6 +2,9 @@ package com.zeus.App.Controller;
 
 import com.zeus.App.SearchManager;
 import com.zeus.utils.api.APIHandler;
+import com.zeus.utils.background.BackgroundTask;
+import com.zeus.utils.log.Logger;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -9,6 +12,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Translate implements Initializable {
@@ -20,20 +24,37 @@ public class Translate implements Initializable {
     Button translate;
     @FXML
     Button erase;
+    final String[] meaning = new String[1];
     public void Translate() {
+        word.setOnKeyReleased(keyEvent -> {
+            BackgroundTask.perform(()->{
+                List<String> translations = APIHandler.translate(word.getText());
+                if (translations.isEmpty()) {
+                    meaning[0] = "Sa rang hê ô";
+                } else {
+                    meaning[0] = translations.get(0).toString();
+                }
+            });
+        });
         translate.setOnMouseClicked(e -> {
             if (word.getText().length() == 0) {
                 result.setVisible(true);
                 result.setText("Sa rang hê ô");
             } else {
                 if (SearchManager.search(word.getText())) {
-                    SceneContainer sc = SceneContainer.sceneContainer;
-                    Label label = new Label(word.getText().toLowerCase());
-                    WordView.setMenuLabel(label);
-                    sc.changeView("/com/zeus/fxml/WordView.fxml");
+                    Platform.runLater(() -> {
+                        SceneContainer sc = SceneContainer.sceneContainer;
+                        Label label = new Label(word.getText().toLowerCase());
+                        WordView.setMenuLabel(label);
+                        sc.changeView("/com/zeus/fxml/WordView.fxml");
+                    });
                 } else {
-                    result.setVisible(true);
-                    result.setText(APIHandler.translate(word.getText()).get(0).toString());
+                    Platform.runLater(() -> {
+                        //List<String> translations = APIHandler.translate(word.getText());
+                        //meaning[0] = translations.get(0).toString();
+                        result.setVisible(true);
+                        result.setText(meaning[0]);
+                    });
                 }
             }
         });
