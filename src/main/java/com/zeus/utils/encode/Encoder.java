f8a4270de31b8ea3afbc30a7d404fb56dabfdd0e
MinhSpaceX <126952602+MinhSpaceX@users.noreply.github.com>
2023-11-24 21:45:31 +0700

    Delete src/main directory

--- src/main/java/com/zeus/utils/encode/Encoder.java
+++
@@ -1,9 +0,0 @@
-package com.zeus.utils.encode;
-
-import java.util.UUID;
-
-public class Encoder {
-    public static String encode(String word) {
-        return UUID.nameUUIDFromBytes(word.getBytes()).toString();
-    }
-}
